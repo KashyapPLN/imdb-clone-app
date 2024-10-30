254f00a2c53a2d30cc3aae7e367f7cbb17d9776b
KashyapPLN <kashyap.pln@gmail.com>
2024-10-30 14:40:02 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,26 +10,30 @@ import { Button, Modal } from 'react-bootstrap';
 import Login from '../login/Login';
 import Signup from '../login/Signup';
 import { useNavigate } from 'react-router-dom';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { MdOutlineAddBox } from 'react-icons/md';
 import AddMovie from './AddMovie';
+import AddPerson from './AddPerson';
 
-export default function NavbarComponent({userName,setUserName}) {
+export default function NavbarComponent({ userName, setUserName }) {
   const [show, setShow] = useState(false);
   const [showAdd, setShowAdd] = useState(false);
+  const [showAddPerson, setShowAddPerson] = useState(false);
   const [login, setLogin] = useState(true);
   const handleShow = () => { setShow(true); setLogin(true) };
   const handleClose = () => setShow(false);
   const handleAddShow = () => setShowAdd(true);
   const handleAddClose = () => setShowAdd(false);
+  const handleAddPersonShow = () => setShowAddPerson(true);
+  const handleAddPersonClose = () => setShowAddPerson(false);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(localStorage.getItem('token')){
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
       const decodedToken = jwtDecode(localStorage.getItem('token'));
       setUserName(decodedToken.name);
     }
-  },[])
+  }, [])
 
   function logout() {
     localStorage.clear();
@@ -42,8 +46,8 @@ export default function NavbarComponent({userName,setUserName}) {
         <Container>
           <Navbar.Brand href="/">
             {/* <img className='imdb-icon' src={imdbLogo} /> */}
-            <span style={{color:"gold",fontWeight:800}}>Imdb-Clone</span>
-            </Navbar.Brand>
+            <span style={{ color: "gold", fontWeight: 800 }}>Imdb-Clone</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
@@ -54,8 +58,8 @@ export default function NavbarComponent({userName,setUserName}) {
 
             </Nav>
             <Nav>
-            {localStorage.getItem('token') && <Button variant='outline-light' className='me-4' style={{fontWeight:700}} onClick={handleAddShow}>Add movie</Button>}
-
+              {localStorage.getItem('token') && <Button variant='outline-light' className='me-4' style={{ fontWeight: 700 }} onClick={handleAddShow}>Add movie</Button>}
+              {localStorage.getItem('token') && <Button variant='outline-light' className='me-4' style={{ fontWeight: 700 }} onClick={handleAddPersonShow}>Add person</Button>}
               {!localStorage.getItem('token') ? <Button variant='text' style={{ fontWeight: 700, color: 'white', cursor: 'pointer' }} onClick={handleShow}>Sign In</Button> :
                 <NavDropdown title={<span style={{ fontWeight: 700, color: 'white' }}>{userName}</span>} id="collapsible-nav-dropdown">
                   <NavDropdown.Item onClick={logout}>logout</NavDropdown.Item>
@@ -68,18 +72,26 @@ export default function NavbarComponent({userName,setUserName}) {
         <Modal.Header style={{ borderBottom: 'none' }} closeButton>
           <Modal.Title style={{ color: 'white' }}>{login ? 'Login' : 'Create Account'}</Modal.Title>
         </Modal.Header>
-        {login ? <Login setLogin={setLogin} handleClose={handleClose} userName={userName} setUserName={setUserName}/>
+        {login ? <Login setLogin={setLogin} handleClose={handleClose} userName={userName} setUserName={setUserName} />
           : <Signup setLogin={setLogin} handleClose={handleClose} />
         }
       </Modal>
-      <Modal show={showAdd} onHide={handleAddClose}  centered bg="dark" data-bs-theme="dark">
-        <Modal.Header style={{borderBottom:'none'}} closeButton>
-          <Modal.Title style={{color:'white'}}>Add Movie</Modal.Title>
+      <Modal show={showAdd} onHide={handleAddClose} centered bg="dark" data-bs-theme="dark">
+        <Modal.Header style={{ borderBottom: 'none' }} closeButton>
+          <Modal.Title style={{ color: 'white' }}>Add Movie</Modal.Title>
         </Modal.Header>
-        <Modal.Body style={{color:'white'}}>
-          <AddMovie/>
+        <Modal.Body style={{ color: 'white' }}>
+          <AddMovie />
         </Modal.Body>
-        </Modal>
+      </Modal>
+      <Modal show={showAddPerson} onHide={handleAddPersonClose} centered bg="dark" data-bs-theme="dark">
+        <Modal.Header style={{ borderBottom: 'none' }} closeButton>
+          <Modal.Title style={{ color: 'white' }}>Add Person</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ color: 'white' }}>
+<AddPerson/>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
